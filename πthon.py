06533e5ce3b91828c17f1f83baadac4e56c4d150
ryo11aori-ship-it <ryo11aori@gmail.com>
2026-03-17 21:46:36 +0900

    πthon.py を更新

--- πthon.py
+++ πthon.py
@@ -1,10 +1,16 @@
 import math, itertools
 
+M = lambda f, t: ">"*(t-f) if t>f else "<"*(f-t)
+
+chk = lambda vp, tgt, fp, t1=5, t2=6: M(0, vp) + "[-" + M(vp, t1) + "+" + M(t1, t2) + "+" + M(t2, vp) + "]" + M(vp, t1) + "[-" + M(t1, vp) + "+" + M(vp, t1) + "]" + M(t1, t2) + "-"*tgt + M(t2, fp) + "+" + M(fp, t2) + "[" + M(t2, fp) + "[-]" + M(fp, t2) + "[-]]" + M(t2, 0)
+
 P = lambda txt: "".join("<"*1017 + "".join("+"*v+">" for v in reversed(c)) + "<.[-]" + "<[-]"*6 + ">"*1017 for char in txt for c in [next(c for c in itertools.product(range(4),repeat=7) if int(math.floor(sum(v*(math.pi**k) for k,v in enumerate(c))))%256==ord(char))])
 
-C = lambda tgt,txt: ">[-]>[-]<<[>+>+<<-]>>[<<+>>-]<"+"+"*((4-tgt)%4)+">+<[>-<[-]]>[[-]>>>>>>>>>>"+P(txt)+"<<<<<<<<<<]<<"
+ifa = lambda f1, f2, txt: M(0, f1) + "[" + M(f1, f2) + "[" + M(f2, 0) + P(txt) + M(0, f2) + "[-]]" + M(f2, f1) + "[-]]" + M(f1, 0)
 
-code = ",[<" + C(3,"t[p]++;") + C(1,"t[p]--;") + C(2,"p--;") + C(0,"p++;") + ">,]"
+C = lambda d0, d2, txt: chk(1, d0, 7) + chk(3, d2, 8) + ifa(7, 8, txt)
+
+code = ",[" + C(3,2,"t[p]=(t[p]+1)%4;") + C(1,2,"t[p]=(t[p]+3)%4;") + C(2,3,"p--;") + C(0,3,"p++;") + C(2,2,"putchar(t[p]);") + C(0,2,"t[p]=getchar();") + C(3,1,"while(t[p]!=0){") + C(1,1,"}") + ",]"
 
 c_src = ["#include <stdio.h>\n#include <math.h>\nint main(){int t[20000]={0}; int p=9000; int ch;"]
 
@@ -16,4 +22,4 @@ f = open("pi_out.c", "w")
 f.write("\n".join(c_src))
 f.close()
 
-print("SUCCESS: The Decoupled πthon Compiler Generated!")
+print("SUCCESS: The Ultimate 8-Command πthon Compiler Generated!")
